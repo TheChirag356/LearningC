77aaaf33cccc74e991b55175c95fbc48884ec375
Chirag Arora <54665586+TheChirag356@users.noreply.github.com>
2023-01-08 20:35:13 +0530

    2 numbers - a & b, are written in a file. Write a program to replace them with their sum.

---
+++ replacewithsum.c
@@ -0,0 +1,21 @@
+// 2 numbers - a & b, are written in a file. Write a program to replace them with their sum.
+
+#include<stdio.h>
+
+int main(){
+    FILE *fptr;
+    fptr = fopen("sum.txt", "r");   
+
+    int a;
+    fscanf(fptr, "%d", &a);
+    int b;
+    fscanf(fptr, "%d", &b);
+
+    fclose(fptr);
+
+    fptr = fopen("sum.txt", "w");   
+    fprintf(fptr, "%d", a+b);
+    fclose(fptr);
+
+    return 0;
+}
