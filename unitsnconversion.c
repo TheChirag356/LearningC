76d527ae78f2081d0ce57b20034a940c7663ea03
Chirag Arora <54665586+TheChirag356@users.noreply.github.com>
2023-01-13 18:10:48 +0530

    Changed just a bit

--- unitsnconversion.c
+++ unitsnconversion.c
@@ -8,23 +8,26 @@ inches to meters
 
 */
 
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
+int main()
+{
     char input;
     float kmsToMiles = 0.621371;
     float inchesToFoot = 0.0833333;
     float cmsToInches = 0.393701;
     float poundToKgs = 0.453592;
     float inchesToMeters = 0.0254;
+    float first, second;
 
-
-
-    while(1)
+    while (1)
     {
         printf("\n1. Kms to Miles\n2. Inches to Foot\n3. Cms to Foot\n4. Pounds to Kgs\n5. Inches to Meters\n");
         printf("Enter the input characters(q to quit): ");
-        scanf("%c", &input);
+        scanf(" %c", &input);
+
+        printf("\nEnter quantity in term of first unit: ");
+        scanf("%f", &first);
 
         switch (input)
         {
@@ -32,16 +35,38 @@ int main(){
             printf("Quitting the program......");
             goto end;
             break;
-        case '':
-        break;
+
+        case '1':
+            second = first * kmsToMiles;
+            printf("%f Kms is equals to Miles %f \n", first, second);
+            break;
+        
+        case '2':
+            second = first * inchesToFoot;
+            printf("%f Inches is equals to %f Foot\n", first, second);
+            break;
+        
+        case '3':
+            second = first * cmsToInches;
+            printf("%f Cms is equals to %f Inches\n", first, second);
+            break;
+        
+        case '4':
+            second = first * poundToKgs;
+            printf("%f Pounds is equals to %f Kgs \n", first, second);
+            break;
+        
+        case '5':
+        second = first * inchesToMeters;
+            printf("%f Inches is equals to %f Meters\n", first, second);
+            break;
+        
 
         default:
             break;
         }
-
     }
-    end:
-
+end:
 
     return 0;
 }
