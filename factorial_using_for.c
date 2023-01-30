2d712e41045ca2cfbabd01be1392f1fdd38e1912
Chirag Arora <54665586+TheChirag356@users.noreply.github.com>
2023-01-30 21:50:31 +0530

    Write a program to find factorial of a number.

---
+++ factorial_using_for.c
@@ -0,0 +1,21 @@
+//Write a program to find factorial of a number.
+
+#include <stdio.h>
+int main() {
+    int n;
+    unsigned long long fact = 1;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+
+    // shows error if the user enters a negative integer
+    if (n < 0)
+        printf("Error! Factorial of a negative number doesn't exist.");
+    else {
+        for (int i = 1; i <= n; ++i) {
+            fact *= i;
+        }
+        printf("Factorial of %d = %llu", n, fact);
+    }
+
+    return 0;
+}
